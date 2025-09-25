78b206c66b2913c93b3a8305efb4f90684be9d0f
motonum <73626946+motonum@users.noreply.github.com>
2025-09-25 19:12:20 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,132 @@
+const DISCORD_WEBHOOK_URL =
+  PropertiesService.getScriptProperties().getProperty("DISCORD_WEBHOOK_URL");
+
+const CALENDAR_ID =
+  PropertiesService.getScriptProperties().getProperty("CALENDAR_ID");
+
+
+class ExDate extends Date {
+  toFormattedString() {
+    return this.toLocaleString("ja-JP", {
+      month: "short",
+      day: "numeric",
+      weekday: "short"
+    })
+  }
+  omitTime() {
+    this.setHours(0, 0, 0, 0);
+    return this;
+  }
+  shiftDate(days = 1) {
+    this.setDate(this.getDate() + days);
+    return this;
+  }
+  getWeekdayName() {
+    return ["日", "月", "火", "水", "木", "金", "土"][this.getDay()];
+  }
+  toDate() {
+    return new Date(this.getTime());
+  }
+  static isSameDay(day1, day2) {
+    return day1.getFullYear() === day2.getFullYear()
+      && day1.getMonth() === day2.getMonth()
+      && day1.getDate() === day2.getDate();
+  }
+}
+
+function refreshTrigger() {
+  const allTriggers = ScriptApp.getProjectTriggers();
+  allTriggers.filter(trigger => trigger.getHandlerFunction() === "main").forEach(trigger => {
+    ScriptApp.deleteTrigger(trigger);
+  })
+
+  const now = new ExDate();
+
+  const year = now.getFullYear();
+  const month = now.getMonth();
+  const date = now.getDate();
+  const hour = 8;
+  const minute = 0;
+
+  const next = new ExDate(year, month, date, hour, minute);
+
+  if (now.getTime() >= next.getTime()) {
+    next.shiftDate();
+  }
+
+  ScriptApp.newTrigger("main").timeBased().at(next.toDate()).create();
+}
+
+// Discordに通知
+function notify(url, message) {
+  UrlFetchApp.fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    payload: JSON.stringify({ content: message }),
+  });
+}
+
+// 指定日の予定タイトルを取得
+function getEventTitles(date) {
+  const myCalendar = CalendarApp.getCalendarById(CALENDAR_ID);
+  const events = myCalendar.getEventsForDay(date);
+  const eventTitles = events.map(event => event.getTitle());
+
+  return eventTitles;
+}
+
+// 通知メッセージを作成
+function makeDailyNotificationMessage(eventTitles) {
+  if (eventTitles.length === 0) return null;
+  const eventListString = eventTitles.map(
+    eventTitle => `- ${eventTitle}`
+  ).join("\n");
+  return `本日の予定はこちらです\n${eventListString}`
+}
+
+// 週の通知メッセージを作成
+function makeWeeklyNotificationMessage(startAt) {
+  const startDate = new ExDate(startAt.getTime()).omitTime();
+  const endDate = new ExDate(startAt.getTime()).omitTime().shiftDate(7);
+
+  const myCalendar = CalendarApp.getCalendarById(CALENDAR_ID);
+  const events = myCalendar.getEvents(startDate, endDate);
+  if (!events.length) return null;
+  const dates = events
+    .filter(event => {
+      if (!event.isAllDayEvent()) return true;
+      const start = new ExDate(event.getStartTime());
+      const end = new ExDate(event.getEndTime()).shiftDate(-1);
+      return ExDate.isSameDay(start, end);
+    })
+    .map(event => new ExDate(event.getStartTime()).omitTime().getTime());
+  
+  if (!dates.length) return null;
+
+  const deduplicatedDates = [... new Set(dates)];
+  const sortedDateStrings = deduplicatedDates.toSorted((a,b) => a - b).map(time => new ExDate(time).toFormattedString())
+  const groupedEvents = Map.groupBy(events, (event) => new ExDate(event.getStartTime()).toFormattedString());
+
+  const message = sortedDateStrings
+    .map(dateString => dateString + "\n- " + groupedEvents.get(dateString).map(event => event.getTitle()).join("\n- "))
+    .join("\n\n");
+
+  return `今週の予定はこちらです\n\n${message}`;
+}
+
+// メイン関数
+function main() {
+  const now = new ExDate();
+  // 月曜は週の予定を通知
+  if (now.getWeekdayName() === "月") {
+    const message = makeWeeklyNotificationMessage(now);
+    if (!message) return;
+    notify(DISCORD_WEBHOOK_URL, message);
+  } else {
+    const message = makeDailyNotificationMessage(getEventTitles(now));
+    if (!message) return;
+    notify(DISCORD_WEBHOOK_URL, message);
+  }
+}
